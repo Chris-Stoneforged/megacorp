816b6e67dfc39bae4938487de49f718b843a2f2e
Christopher Stone <christopher.stone@stoneforgedgames.com>
2024-11-29 11:14:55 +1300

    P: Revert Kq

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,4 +1,1 @@
-echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "======== CREDIT CARD SCAN COMPLETE ========="
-grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+# TODO: write the script
